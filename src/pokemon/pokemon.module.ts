b077a1af62b964967237b3f3e06ba458fd9e6e4d
Rene Pablo <renepablo@gmail.com>
2022-10-19 23:02:34 -0400

    Fin seccion 7

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, pokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: pokemonSchema,
+      }
+    ])
+  ]
+})
+export class PokemonModule {}
